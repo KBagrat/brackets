9d22b727f25c1a7f1eebed5c77755f6e784f7d22
bagrat <k.bagrat@gmail.com>
2023-02-24 21:47:52 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let brackets = bracketsConfig.map(el => el[0] + el[1]); // [ '12', '34', '56', '77', '88' ]
+	let result = [...str].join(''); //'111115611111111222288888822225577877778775555666677777777776622222'
+
+	for (let i = 0; i < brackets.length; i++) {
+		while (result.includes(brackets[i])) {
+			let index = result.indexOf(brackets[i]);
+			result = result.slice(0, index) + result.slice(index + 2);
+		}
+	}
+	for (let i = 0; i < brackets.length; i++) {
+		while (result.includes(brackets[i])) {
+			let index = result.indexOf(brackets[i]);
+			result = result.slice(0, index) + result.slice(index + 2);
+		}
+	}
+	for (let i = 0; i < brackets.length; i++) {
+		while (result.includes(brackets[i])) {
+			let index = result.indexOf(brackets[i]);
+			result = result.slice(0, index) + result.slice(index + 2);
+		}
+	}
+	for (let i = 0; i < brackets.length; i++) {
+		while (result.includes(brackets[i])) {
+			let index = result.indexOf(brackets[i]);
+			result = result.slice(0, index) + result.slice(index + 2);
+		}
+	}
+	for (let i = 0; i < brackets.length; i++) {
+		while (result.includes(brackets[i])) {
+			let index = result.indexOf(brackets[i]);
+			result = result.slice(0, index) + result.slice(index + 2);
+		}
+	}
+
+	return !(result.length);
+};
